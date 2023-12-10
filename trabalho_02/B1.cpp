ddfbb0b421b24a9143548610c595f0b0c446a226
Bruno Ribeiro <bbrunoo@icloud.com>
2023-12-10 14:02:50 -0300

    Substituição de páginas: FIFO

---
+++ trabalho_02/B1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <deque>
+
+int main() {
+	int q, pages, pickpage, page_faults = 0;
+	scanf("%d\n%d\n%d", &q, &pages, &pickpage);
+	std::deque<int> qq;
+	qq.push_back(pickpage);
+	page_faults++, pages--;
+	while (pages) {
+		scanf("%d", &pickpage);
+		if (std::find(qq.begin(), qq.end(), pickpage) == qq.end()) {
+			if (qq.size() != q)
+				qq.push_back(pickpage);
+			else{
+				qq.pop_front();
+				qq.push_back(pickpage);
+			}
+			page_faults++;
+		}
+		pages--;
+	}
+	printf("%d\n", page_faults);
+	return 0;
+}
